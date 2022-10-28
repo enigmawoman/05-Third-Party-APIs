6f1796160632571c79bad93ab7006d9bf7abc88c
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2022-10-28 18:53:42 +0100

    main JS code completed to create a functioning calendar, refinement and comments required, as well as additions to bootstrap styling and CSS

--- assets/style.js
+++ assets/style.js
@@ -9,46 +9,173 @@
 
 var calendarHours = {
     dayStart: 9,
-    dayEnd: 17,
+    dayEnd: 20,
 };
+
+
+
+var calendarGrid = $('.container');
+
+
 
 let currentDay = function() {
     document.querySelector("#currentDay").innerHTML =`
-    <h5>${moment().format('MMMM Do YYYY, h:mm:ss a')}</h5>`;
-    //$("#currentDay").text(currentDay);
+    <h5>${moment().format('MMMM Do YYYY, HH:mm:ss a')}</h5>`;
+
 };
+
+
 
 init();
 
+
 function init() {
+
+    generateCalSlots();
+    calRefresh();
+    
 
     var dayOfWeek = moment().format('dddd');
     $("#dow").text(' 📅 ' + dayOfWeek + ' 📅 ');
     clock();
-    //can also add in the time poller beneath this to update the time slots - as per Jasons example
+
+    setInterval(calRefresh, 10000);
+    
+   
 };
+
+
 
 function clock() {
     currentDay();
     setInterval(currentDay, 1000);
 };
 
+
+
 function generateCalSlots() {
 
+    
+
+    for (var hour = calendarHours.dayStart; hour <= calendarHours.dayEnd; hour++) {
+
+        var loadSavedItems = localStorage.getItem(hour);
+
+        var timeBlocks = $('<div>');
+        timeBlocks.addClass('row mb-3 text center time-block');
+        timeBlocks.attr('hour-slot', hour);
+        calendarGrid.append(timeBlocks);
+
+        var timeOfDay = $('<div>').addClass('col-lg-2 themed-grid-col hour');
+        timeOfDay.text(moment(hour, 'h').format('HHmm'));
+        timeBlocks.append(timeOfDay);
+
+        var calBlock = $('<div>').addClass('col-lg-9 cal-block themed-grid-col');
+        //calBlock.attr('hour-slot', hour);
+        var calInput = $('<textarea>').addClass('col-12 description');
+        timeBlocks.append(calBlock);
+        calBlock.append(calInput);
+        calInput.val(loadSavedItems);
+
+        //how do i get the text in the button to align center vertical without changing the size of the button
+        var saveBtn = $('<div>').addClass('col-lg-1 themed-grid-col btn btn-primary saveBtn');
+        saveBtn.text('💾  SAVE').addClass('font-weight-bold text-center');
+        timeBlocks.append(saveBtn);
+        saveBtn.on("click", saveItem);
+
+    }
+
+    //need to add in a getItems from local storage
 };
 
-function loadSavedItems() {
+
+
+function saveItem(event) {
+
+  event.preventDefault();
+ console.log($(this).parent().attr('hour-slot'));
+ // need two variables hour and task to save to LS
+ var hour = $(this).parent().attr('hour-slot');
+
+ 
+ var task = $(this).siblings( ".cal-block" ).children().val();
+
+ localStorage.setItem(hour, task);
+
+
+
+
+    // var saveInput = calInput.val();
+
+    //      if (saveInput === '') {
+    //          window.confirm("You must input text!");
+    //      } else {
+    //          localStorage.setItem("calInput");
+    //      };
+
+    
 
 };
 
-function saveItem() {
 
-};
 
 function calRefresh() {
+    var currentTime = moment().hour();
+   //console.log(currentTime);
+    
 
-};
+      $('.time-block').each(function (index, element) {
+
+              var hour = $(element).attr('hour-slot');
+              //console.log(hour);
+
+              //console.log($(this));
+
+              if (hour < currentTime) {
+                $(this).addClass('past');
+               //$('.cal-block').addClass('past');
+    //             //$('container').children().addClass('past');
+    //             // $(element).find('cal-block').addClass('past');
+
+            } else if (hour == currentTime) {    
+                    $(this).addClass('present');
+    //              //$(element).find('cal-block').addClass('present');
+
+              } else {
+                     $(this).addClass('future');
+                
+    //              //$(element).find('cal-block').addClass('future');
+              }
+         });
+        
+    //     }
+        
+
+    //   var currentTime = moment().hour();
+    //   console.log(currentTime);
+
+    //   for (var hour = calendarHours.dayStart; hour <= calendarHours.dayEnd; hour++) {
+
+    //     console.log(hour);
+
+    //                 if (hour < currentTime) {
+    //                      $('.cal-block').addClass('past');
+    //                     // $('.time-block').children(1).addClass('past');
+    //                       //$(element).find('cal-block').addClass('past');
+        
+    //                   } else if (hour == currentTime) {
+    //                      $('.cal-block').addClass('present');
+    //                     // $('.time-block').children(1).addClass('present');
+    //                      // $(element).find('cal-block').addClass('present');
+        
+    //                   } else {
+    //                      $('.cal-block').addClass('future');
+    //                     // $('.time-block').children(1).addClass('future');
+    //                     //  $(element).find('cal-block').addClass('future');
+    //                   }
+    //   }
+    }
 
 // add the getItems into the for loop for generating the calendar slots
 
-//build the bootstrap styles in HTML and then use those stylings to then use in the dynamic styling in the ger
+//build the bootstrap styles in HTML and then use those stylings to then use in the dynamic styling in the g
